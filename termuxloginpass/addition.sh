69f1334fe04d27438d43e571e5a1bc4496f289a6
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-22 18:16:26 +0400

    Delete termuxloginpass directory

--- termuxloginpass/addition.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-chmod 500 ~/termuxloginpass/login.py
-chmod 500 ~/termuxloginpass/pass.py
-chmod 500 ~/termuxloginpass/chlogin.py
-chmod 500 ~/termuxloginpass/chpass.py
-chmod 500 ~/termuxloginpass
