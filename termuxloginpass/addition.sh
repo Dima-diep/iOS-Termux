37f6e164ce46a07e42981e0a4c82b1755d33cb38
Dima-diep <52601543+Dima-diep@users.noreply.github.com>
2021-07-22 18:13:04 +0400

    addition.sh

---
+++ termuxloginpass/addition.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+chmod 500 ~/termuxloginpass/login.py
+chmod 500 ~/termuxloginpass/pass.py
+chmod 500 ~/termuxloginpass/chlogin.py
+chmod 500 ~/termuxloginpass/chpass.py
+chmod 500 ~/termuxloginpass
